9e4bf637397f4edee622f8febe4437c3a4ae03b4
Gustavo <gumastro@usp.br>
2022-09-06 22:29:24 -0300

    Feat: NFT contract
    Implement and deploy ERC721 standard contract to mint NFTs. Also save their info to use on frontend

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -6,10 +6,12 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // deploy contracts here:
-  
-  
+  const NFT = await ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy();
+  console.log("NFT contract address", nft.address);
+
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  saveFrontendFiles(nft, "NFT");
 }
 
 function saveFrontendFiles(contract, name) {
