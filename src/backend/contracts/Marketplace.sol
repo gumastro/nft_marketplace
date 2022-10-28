38bafb783d9831e12fba0a9debe689e69f600359
Gustavo <gumastro@usp.br>
2022-10-28 18:45:15 -0300

    Feat: Add NFT item to marketplace contract
    Send NFT to contract and emit offered (for a set price) event

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -20,10 +20,43 @@ contract Marketplace is ReentrancyGuard {
         bool sold;
     }
 
+    event Offered (
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller
+    );
+
     mapping(uint => Item) public items;
 
     constructor (uint _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero");
+        itemCount++;
+
+        _nft.transferFrom(msg.sender, address(this), _tokenId); // transfer nft
+        
+        // add new item to items mapping
+        items[itemCount] = Item (
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+
+        emit Offered(
+            itemCount,
+            address(_nft),
+            _tokenId,
+            _price,
+            msg.sender
+        );
     }
 }
