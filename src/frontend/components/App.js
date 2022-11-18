1c44822d227d09a793ad1e7705165e64e9fe96d0
Gustavo <gumastro@usp.br>
2022-11-18 18:00:42 -0300

    Feat: Front-end setup
    Setup navbar, web3handler, contract loading and routing

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,27 +1,70 @@
 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
 import logo from './logo.png';
 import './App.css';
+import Navigation from './Navbar'
+import { useState } from 'react'
+import { ethers } from "ethers"
+import MarketplaceAbi from '../contractsData/Marketplace.json'
+import MarketplaceAddress from '../contractsData/Marketplace-address.json'
+import NFTAbi from '../contractsData/NFT.json'
+import NFTAddress from '../contractsData/NFT-address.json'
+import Home from './Home'
+import Create from './Create'
+import MyListedItems from './MyListedItems'
+import MyPurchases from './MyPurchases'
+import { Spinner } from 'react-bootstrap'
  
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [account, setAccount] = useState(null)
+  const [nft, setNFT] = useState({})
+  const [marketplace, setMarketplace] = useState({})
+  // Metamask login/connect
+  const web3Handler = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0])
+    // get provider from Metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+
+    loadContracts(signer)
+  }
+
+  const loadContracts = async (signer) => {
+    // get deployed copies of contracts
+    const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer)
+    setMarketplace(marketplace)
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer)
+    setNFT(nft)
+    setLoading(false)
+  }
+
   return (
-    <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        NFT Marketplace
-      </nav>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <img src={logo} className="App-logo" alt="logo"/>
-              <h1 className= "mt-5">NFT Marketplace Catto</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>
-            </div>
-          </main>
-        </div>
+    <BrowserRouter>
+      <div>
+        <Navigation web3Handler={web3Handler} account={account} />
+        { loading ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
+            <Spinner animation="border" style={{ display: 'flex' }} />
+            <p className='mx-3 my-0'>Awaiting Metamask Connection...</p>
+          </div>
+        ) : (
+          <Routes>
+            <Route path="/" element={ 
+              <Home />
+            } />
+            <Route path="/create" />
+            <Route path="/my-listed-items" />
+            <Route path="/my-purchases" />
+          </Routes>
+        )}
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
